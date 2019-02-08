cfbeddb5b584bb40689b80aa97d3b3837a91fc22
Brian Lagman <bjlagman@gmail.com>
2019-02-08 15:14:17 -0800

    Update schema for foreign key

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE neighborhood;
 USE neighborhood;
 
 CREATE TABLE property (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   address1 VARCHAR(32),
   city VARCHAR(32),
   zip INT,
@@ -13,17 +13,16 @@ CREATE TABLE property (
   bed_count INT,
   bath_count INT,
   pic_count INT,
-  house_status BOOLEAN default 0
+  house_status BOOLEAN default 0,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE location (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT,
   property_id INT,
   zip INT,
   walk_score INT,
-  transit_score INT
+  transit_score INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (property_id) REFERENCES property(id)
 )
-
--- INSERT into property(address1, city, zip, price, bed_count, bath_count, pic_count, house_status) VALUES()
-
--- INSERT into location (property_id, zip, walk_score, transit_score) VALUES(123,321,34,32)
