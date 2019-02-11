f75963a34a443bc6e643d48ed97df527d1046227
Brian Lagman <bjlagman@gmail.com>
2019-02-11 14:46:20 -0800

    Add component

---
+++ src/component/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+  render(){
+    return(
+      <div>
+        <h1>this is working</h1>
+        <img src="property1.jpg"></img>
+      </div>
+    )
+  }
+}
+
+export default App;
+
